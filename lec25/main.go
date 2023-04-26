c52a4655b8a73c6724b1f86ff0653933801f2f3a
Syful Sharif <sharif.syful@gmail.com>
2023-04-26 20:01:29 +0600

    Multiple go files created to make the project moduler

--- lec25/main.go
+++ lec25/main.go
@@ -3,20 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"text/template"
 )
 
 const portNumber = ":8080"
-
-// Home is the homepage Handler of this web app!
-func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplates(w, "home.page.tmpl")
-}
-
-// About is the about page handler
-func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplates(w, "about.page.tmpl")
-}
 
 // main is the entry point of the application
 func main() {
@@ -25,14 +14,4 @@ func main() {
 
 	fmt.Println(fmt.Sprintf("Application is running at localhost%s", portNumber))
 	http.ListenAndServe(portNumber, nil)
-}
-
-func renderTemplates(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-
-	if err != nil {
-		fmt.Println("error parsing template", err)
-		return
-	}
 }
