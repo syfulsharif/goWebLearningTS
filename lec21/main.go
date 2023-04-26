1335bd84f4aa2c54e9ddecde44679c656e2932fe
Syful Sharif <sharif.syful@gmail.com>
2023-04-26 06:35:39 +0600

    Route Handling in Go is done

--- lec21/main.go
+++ lec21/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -23,10 +24,32 @@ func addValues(x, y int) int {
 	return x + y
 }
 
+func Divide(w http.ResponseWriter, r *http.Request) {
+	f, err := divideValues(100.0, 0.0)
+
+	if err != nil {
+		fmt.Fprintf(w, "Can not divide by 0.0")
+		return
+	}
+
+	fmt.Fprintf(w, fmt.Sprintf("<h2>%f divided by %f is %f</h2>", 100.0, 0.0, f))
+}
+
+func divideValues(x, y float32) (float32, error) {
+	if y <= 0 {
+		err := errors.New("can not divide by zero")
+		return 0, err
+	}
+	result := x / y
+	return result, nil
+}
+
 // main is the entry point of the application
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
+
 	fmt.Println(fmt.Sprintf("Application is running at localhost%s", portNumber))
 	http.ListenAndServe(portNumber, nil)
 }
