bc6fbc71c62546359ca7e714e1019943178c9fd3
Syful Sharif <sharif.syful@gmail.com>
2023-04-29 09:59:34 +0600

    Go Modules Added

---
+++ lec25/cmd/web/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"helloTemplate/handlers"
+	"net/http"
+)
+
+const portNumber = ":8080"
+
+// main is the entry point of the application
+func main() {
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
+
+	fmt.Println(fmt.Sprintf("Application is running at localhost%s", portNumber))
+	http.ListenAndServe(portNumber, nil)
+}
