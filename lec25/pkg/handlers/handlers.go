73d243782f03b8f64494372de272cd12f71b8c2f
Syful Sharif <sharif.syful@gmail.com>
2023-05-01 12:30:07 +0600

    Template Cache system added

--- lec25/pkg/handlers/handlers.go
+++ lec25/pkg/handlers/handlers.go
@@ -7,10 +7,10 @@ import (
 
 // Home is the homepage Handler of this web app!
 func Home(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplates(w, "home.page.tmpl")
+	render.RenderTemplate(w, "home.page.tmpl")
 }
 
 // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplates(w, "about.page.tmpl")
+	render.RenderTemplate(w, "about.page.tmpl")
 }
