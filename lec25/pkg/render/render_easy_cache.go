b728980c497b09eb3b62a26e3115032bd1d41b5d
Syful Sharif <sharif.syful@gmail.com>
2023-05-04 10:57:34 +0600

    Complex but efficient Caching of template system added

---
+++ lec25/pkg/render/render_easy_cache.go
@@ -0,0 +1,67 @@
+// package render
+
+// import (
+// 	"fmt"
+// 	"html/template"
+// 	"log"
+// 	"net/http"
+// )
+
+// func RenderTemplateTest(w http.ResponseWriter, tmpl string) {
+// 	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
+// 	err := parsedTemplate.Execute(w, nil)
+
+// 	if err != nil {
+// 		fmt.Println("error parsing template", err)
+// 		return
+// 	}
+// }
+
+// var tc = make(map[string]*template.Template) //tc = Template Cache
+
+// func RenderTemplate(w http.ResponseWriter, t string) {
+// 	var tmpl *template.Template
+// 	var err error
+
+// 	//check to see if we already have the template in our cache
+// 	_, inMap := tc[t]
+
+// 	if !inMap {
+// 		//Need to create the template
+// 		log.Println("creating template and adding to cache")
+// 		err = createTemplateCache(t)
+
+// 		if err != nil {
+// 			log.Println(err)
+// 		}
+// 	} else {
+// 		// We have template in the cache
+// 		log.Println("using cached template")
+// 	}
+
+// 	tmpl = tc[t]
+// 	err = tmpl.Execute(w, nil)
+
+// 	if err != nil {
+// 		log.Println(err)
+// 	}
+
+// }
+
+// func createTemplateCache(t string) error {
+// 	templates := []string{
+// 		fmt.Sprintf("./templates/%s", t),
+// 		"./templates/base.layout.tmpl",
+// 	}
+
+// 	//Parse the template
+// 	tmpl, err := template.ParseFiles(templates...)
+
+// 	if err != nil {
+// 		return err
+// 	}
+// 	// Add template to cache (Map)
+// 	tc[t] = tmpl
+
+// 	return nil
+// }
