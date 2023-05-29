e6b0c4f09c2dca3df30f3e5e2a5095b8a99165b5
Syful Sharif <sharif.syful@gmail.com>
2023-05-29 21:01:14 +0600

    Modules added

---
+++ section_3/cmd/web/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"myWebApp/pkg/handlers"
+	"net/http"
+)
+
+const portNumber = ":8080"
+
+// main is the main entry point of the app
+func main() {
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
+	fmt.Println(fmt.Sprintf("Starting the application on port %s", portNumber))
+	http.ListenAndServe(portNumber, nil)
+}
